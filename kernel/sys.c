d23dcb9b6571480224452e81180395570cdea1c6
ffolkes <ffolkess@gmail.com>
2015-06-12 18:40:25 -0400

    sys: export do_sysinfo()

--- kernel/sys.c
+++ kernel/sys.c
@@ -2478,7 +2478,7 @@ EXPORT_SYMBOL_GPL(orderly_poweroff);
  * do_sysinfo - fill in sysinfo struct
  * @info: pointer to buffer to fill
  */
-static int do_sysinfo(struct sysinfo *info)
+int do_sysinfo(struct sysinfo *info)
 {
 	unsigned long mem_total, sav_total;
 	unsigned int mem_unit, bitcount;
@@ -2540,6 +2540,7 @@ static int do_sysinfo(struct sysinfo *info)
 out:
 	return 0;
 }
+EXPORT_SYMBOL_GPL(do_sysinfo);
 
 SYSCALL_DEFINE1(sysinfo, struct sysinfo __user *, info)
 {
